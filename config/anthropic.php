0e00bc66cfc7ec9d74af7bbf9b46a3c5b5a836f1
Mozex <mozex.gm@gmail.com>
2024-05-01 17:44:41 +0300

    update config file

--- config/anthropic.php
+++ config/anthropic.php
@@ -4,16 +4,15 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | OpenAI API Key and Organization
+    | Anthropic API Key
     |--------------------------------------------------------------------------
     |
-    | Here you may specify your OpenAI API Key and organization. This will be
-    | used to authenticate with the OpenAI API - you can find your API key
-    | and organization on your OpenAI dashboard, at https://openai.com.
+    | Here you may specify your Anthropic API Key. This will be used to authenticate
+    | with the Anthropic API - you can find your API key on your Anthropic
+    | dashboard, at https://console.anthropic.com/settings/keys.
     */
 
-    'api_key' => env('OPENAI_API_KEY'),
-    'organization' => env('OPENAI_ORGANIZATION'),
+    'api_key' => env('ANTHROPIC_API_KEY'),
 
     /*
     |--------------------------------------------------------------------------
@@ -24,5 +23,5 @@ return [
     | for a response. By default, the client will time out after 30 seconds.
     */
 
-    'request_timeout' => env('OPENAI_REQUEST_TIMEOUT', 30),
+    'request_timeout' => env('ANTHROPIC_REQUEST_TIMEOUT', 30),
 ];
